08963448fe733c03202a83f63d54c5488d2d194f
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-09-01 11:38:36 +0600

    do

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -3,6 +3,14 @@ package school.mjc.stage0.loops.finalTask;
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
 
+        for (int i = cathetusLength; i > 1; i--) {
+            for (int j = 1; j < cathetusLength; j++) {
+                if (j<=i){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,11 +1,21 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
-    public void printPyramid(int cathetusLength) {
-
-    }
-
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
+    }
+
+    public void printPyramid(int cathetusLength) {
+
+        for (int i = cathetusLength; i >= 1; --i) {
+            for (int j = cathetusLength; j >= 1; --j) {
+                if (i <= j) {
+                    System.out.print(Math.abs(j));
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
